b03dfd29684cd6dddd8448d0bcb7bf91c5a01a59
Hamsty10 <guilhermescarabelot6@gmail.com>
2024-08-07 09:09:33 -0300

    Add files via upload

---
+++ teste/teste.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class teste{
+    public static void main(Strin[] args){
+        Scanner leia = new Scanner(System.in);
+        float = soma, sub, multi, div, n1, n2 
+
+        System.out.print("digite o primeiro numero");
+        n1 = leia.nextfloat();
+
+        System.out.print("digite o segundo numero");
+        n2 = leia.nextfloat();
+        soma = n1+n2;
+        sub = n1-n2;
+        multi = n1*n2;
+        div = n1/n2; 
+
+        System.out.println("soma :" + soma);
+        System.out.println("subtração :" + sub);
+        System.out.println("multiplicação :" + multi);
+        System.out.println("divisão :"  div);
+        
+        
+
+
+
+
+    }
+}
